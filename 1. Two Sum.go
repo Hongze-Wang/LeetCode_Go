215e36c21266efbecfd5186effc9dcea674ebbde
Steve Wang <wanghongzecn@qq.com>
2021-10-21 22:27:14 +0800

    Update 1. Two Sum.go

--- 1. Two Sum.go
+++ 1. Two Sum.go
@@ -2,33 +2,33 @@
 
 func twoSum(nums []int, target int) []int {
     seen := map[int]int{}
-	res := []int{}
+    res := []int{}
     need := 0
 	
-	for i, val := range nums {
-		need = target-val
-		v, ok := seen[need]
+    for i, val := range nums {
+    	need = target - val
+	v, ok := seen[need]
 
-		if ok {
-			res = append(res, i, v)
-			break
-		}
-		
-		seen[val] = i
-
+	if ok {
+		res = append(res, i, v)
+		break
 	}
+
+	seen[val] = i
+	
+    }
 	return res
 }
 
 // optimized version, more go style
 func twoSum(nums []int, target int) []int {
     seen := map[int]int{}
-	for i, num := range nums {
-		need := target - num
-		if _, ok := seen[need]; ok {
-			return []int{seen[need], i}
-		}
-		seen[num] = i
+    for i, num := range nums {
+	need := target - num
+	if _, ok := seen[need]; ok {
+	    return []int{seen[need], i}
 	}
-	return []int{}
+	seen[num] = i
+    }
+    return []int{}
 }
